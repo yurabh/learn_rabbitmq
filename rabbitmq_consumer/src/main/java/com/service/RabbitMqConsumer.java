e148789828a63abd7e0c472495b6f0453f229e41
yurabh <yurabh1990@gmail.com>
2022-09-10 15:42:23 +0300

    Small changes.

--- rabbitmq_consumer/src/main/java/com/service/RabbitMqConsumer.java
+++ rabbitmq_consumer/src/main/java/com/service/RabbitMqConsumer.java
@@ -5,25 +5,25 @@ import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RabbitMQConsumer {
+public class RabbitMqConsumer {
 
     @RabbitListener(queues = "${rabbitmq.queue.finance}")
     public void receivedMessage(Employee employee) {
-        System.out.println("Message From RabbitMQ: " + employee);
+        System.out.println("Message From RabbitMQ: finance queue " + employee);
     }
 
     @RabbitListener(queues = "${rabbitmq.queue.marketing}")
     public void receivedMessageOne(Employee employee) {
-        System.out.println("Message From RabbitMQ: " + employee);
+        System.out.println("Message From RabbitMQ: marketing queue" + employee);
     }
 
     @RabbitListener(queues = "${rabbitmq.queue.admin}")
     public void receivedMessageTwo(Employee employee) {
-        System.out.println("Message From RabbitMQ: " + employee);
+        System.out.println("Message From RabbitMQ: admin queue" + employee);
     }
 
     @RabbitListener(queues = "${rabbitmq.queue.all}")
     public void receivedMessageAll(Employee employee) {
-        System.out.println("Message From RabbitMQ: " + employee);
+        System.out.println("Message From RabbitMQ: all queue" + employee);
     }
 }
